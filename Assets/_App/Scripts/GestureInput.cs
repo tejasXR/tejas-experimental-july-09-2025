b5d86dae9b355fa303bc392b5d99d2dbe2468316
tejasXR <tejasshroff24@gmail.com>
2025-07-09 14:49:07 -0400

    Creating logic to rotate a cube depending on the direction of a pinch-and-pull mechanic

--- Assets/_App/Scripts/GestureInput.cs
+++ Assets/_App/Scripts/GestureInput.cs
@@ -7,8 +7,10 @@ namespace Tejas
     {
         [SerializeField] private GestureFingerProvider fingerProvider;
         [SerializeField] private GestureFingerData fingerData;
+        [SerializeField] private CubeRotator cubeRotator;
 
         private GestureController _gestureController;
+        private Vector3? _pinchPoint;
 
         private void Awake()
         {
@@ -34,24 +36,40 @@ namespace Tejas
         private void Update()
         {
             _gestureController.RefreshGestureFrame();
+
+            if (_pinchPoint.HasValue)
+            {
+                var pinchDirectionalVector = fingerProvider.IndexTip.position - _pinchPoint.Value;
+                var projectedVector = Vector3.ProjectOnPlane(pinchDirectionalVector, Vector3.up);
+                
+                Debug.DrawLine(_pinchPoint.Value, projectedVector + _pinchPoint.Value, Color.blue);
+
+                var pullLength = projectedVector.magnitude;
+                var crossResult = Vector3.Cross(Vector3.forward, projectedVector);
+                
+                var pinchPullDirection = crossResult.y > 0 ? -1 : 1;
+                cubeRotator.IncrementalRotate(pullLength * pinchPullDirection);
+            }
         }
 
-        private void OnPinchDown(OVRInput.Handedness handedness)
+        private void OnPinchDown(OVRHand hand, Vector3 pinchPosition)
         {
+            _pinchPoint = pinchPosition;
             Debug.Log("Pinched down");
         }
         
-        private void OnPinchRelease(OVRInput.Handedness handedness)
+        private void OnPinchRelease(OVRHand hand)
         {
+            _pinchPoint = null;
             Debug.Log("Pinch released");
         }
 
-        private void OnPalmUp(OVRInput.Handedness handedness)
+        private void OnPalmUp(OVRHand hand)
         {
             Debug.Log("Palm up");
         }
 
-        private void OnLShapeCreated(OVRInput.Handedness handedness)
+        private void OnLShapeCreated(OVRHand hand)
         {
             Debug.Log("L shape created");
         }
