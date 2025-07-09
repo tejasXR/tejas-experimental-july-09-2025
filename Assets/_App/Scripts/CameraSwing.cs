0b5ace2ee827b6b5bca2b7aef565259ef31dd648
tejasXR <tejasshroff24@gmail.com>
2025-07-09 19:06:42 -0400

    Wrapping class within namespace declaration [POST-SESSION]

--- Assets/_App/Scripts/CameraSwing.cs
+++ Assets/_App/Scripts/CameraSwing.cs
@@ -1,31 +1,31 @@
-using System;
-using Tejas.Snap;
 using UnityEngine;
 
-public class CameraSwing : MonoBehaviour
+namespace Tejas.Snap
 {
-    [SerializeField] private float degreeFactor = 90;
-    [SerializeField] private float swingSpeed = 3F;
-    [SerializeField] private float startingYRotation = 45;
-
-    private void Start()
+    public class CameraSwing : MonoBehaviour
     {
-        transform.Rotate(Vector3.up, startingYRotation);
-    }
+        [SerializeField] private float degreeFactor = 90;
+        [SerializeField] private float swingSpeed = 3F;
+        [SerializeField] private float startingYRotation = 45;
 
-    private void Update()
-    {
-        // Swing
-        var additiveDegrees = Mathf.PingPong(Time.time * swingSpeed, degreeFactor);
-        
-        // Add the swing
-        var aggregateRotation = startingYRotation + additiveDegrees;
-        Debug.Log(aggregateRotation);
-        
-        // Swinging
-        var newRotationEuler = new Vector3(0, aggregateRotation, 0);
-        var rotation = Quaternion.Euler(newRotationEuler);
+        private void Start()
+        {
+            transform.Rotate(Vector3.up, startingYRotation);
+        }
 
-        transform.rotation = rotation;
+        private void Update()
+        {
+            // Swing
+            var additiveDegrees = Mathf.PingPong(Time.time * swingSpeed, degreeFactor);
+        
+            // Add the swing
+            var aggregateRotation = startingYRotation + additiveDegrees;
+        
+            // Swinging
+            var newRotationEuler = new Vector3(0, aggregateRotation, 0);
+            var rotation = Quaternion.Euler(newRotationEuler);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 8F);
+        }
     }
 }
