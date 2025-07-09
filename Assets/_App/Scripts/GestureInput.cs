e403c01ad3bea69846bc7e103c9a3526a6cf4fd3
tejasXR <tejasshroff24@gmail.com>
2025-07-09 13:01:24 -0400

    Class to facilitate the recognition of gestures

---
+++ Assets/_App/Scripts/GestureInput.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tejas
+{
+    public class GestureInput : MonoBehaviour
+    {
+        [SerializeField] private GestureFingerProvider fingerProvider;
+        [SerializeField] private GestureFingerData fingerData;
+
+        private GestureController _gestureController;
+
+        private void Awake()
+        {
+            _gestureController = new GestureController(fingerProvider, fingerData);
+            
+            _gestureController.PinchDownCallback += OnPinchDown;
+            _gestureController.PinchReleaseCallback += OnPinchRelease;
+            _gestureController.PalmUpCallback += OnPalmUp;
+            _gestureController.LShapeCreatedCallack += OnLShapeCreated;
+        }
+
+        private void OnDestroy()
+        {
+            if (_gestureController != null)
+            {
+                _gestureController.PinchDownCallback -= OnPinchDown;
+                _gestureController.PinchReleaseCallback -= OnPinchRelease;
+                _gestureController.PalmUpCallback -= OnPalmUp;
+                _gestureController.LShapeCreatedCallack -= OnLShapeCreated;
+            }
+        }
+
+        private void Update()
+        {
+            _gestureController.RefreshGestureFrame();
+        }
+
+        private void OnPinchDown(OVRInput.Handedness handedness)
+        {
+            Debug.Log("Pinched down");
+        }
+        
+        private void OnPinchRelease(OVRInput.Handedness handedness)
+        {
+            Debug.Log("Pinch released");
+        }
+
+        private void OnPalmUp(OVRInput.Handedness handedness)
+        {
+            Debug.Log("Palm up");
+        }
+
+        private void OnLShapeCreated(OVRInput.Handedness handedness)
+        {
+            Debug.Log("L shape created");
+        }
+    }
+}
