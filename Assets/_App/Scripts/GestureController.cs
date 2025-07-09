dd743abd57f8f5528ad8b0fcd73729e50160ac26
tejasXR <tejasshroff24@gmail.com>
2025-07-09 13:01:04 -0400

    Adding script that takes finger transforms and recognizes when gestures are made

---
+++ Assets/_App/Scripts/GestureController.cs
@@ -0,0 +1,105 @@
+using System;
+using UnityEngine;
+
+namespace Tejas
+{
+    public class GestureController
+    {
+        public event Action<OVRInput.Handedness> PinchDownCallback;
+        public event Action<OVRInput.Handedness> PinchReleaseCallback;
+        public event Action<OVRInput.Handedness> PalmUpCallback;
+        public event Action<OVRInput.Handedness> PalmDownCallback;
+        public event Action<OVRInput.Handedness> LShapeCreatedCallack;
+        
+        private readonly GestureFingerProvider _fingerProvider;
+        private readonly GestureFingerData _fingerData;
+
+        private bool _isPinching;
+        private bool _isPalmUp;
+        private bool _isMakingLShape;
+        
+        public GestureController(GestureFingerProvider fingerProvider, GestureFingerData fingerData)
+        {
+            _fingerProvider = fingerProvider;
+            _fingerData = fingerData;
+        }
+
+        public void RefreshGestureFrame()
+        {
+            CheckPinch();
+            CheckPalmOrientation();
+            CheckLShape();
+        }
+
+        private void CheckPinch()
+        {
+            var thumbTip = _fingerProvider.ThumbTip;
+            var indexTip = _fingerProvider.IndexTip;
+
+            if (VectorMathUtils.GetVectorPointDistance(thumbTip.position, indexTip.position) <
+                _fingerData.pinchDistanceThreshold )
+            {
+                if (!_isPinching)
+                {
+                    PinchDownCallback?.Invoke(OVRInput.Handedness.RightHanded);
+                }
+                
+                _isPinching = true;
+            }
+            else
+            {
+                if (_isPinching)
+                {
+                    PinchReleaseCallback?.Invoke(OVRInput.Handedness.RightHanded);
+                }
+                
+                _isPinching = false;
+            }
+        }
+
+        private void CheckPalmOrientation()
+        {
+            var palm = _fingerProvider.Palm;
+
+            if (Vector3.Dot(-palm.up, Vector3.up) > _fingerData.palmUpThreshold)
+            {
+                if (!_isPalmUp)
+                {
+                    PalmUpCallback?.Invoke(OVRInput.Handedness.RightHanded);
+                }
+                
+                _isPalmUp = true;
+            }
+            else
+            {
+                _isPalmUp = false;
+            }
+        }
+
+        private void CheckLShape()
+        {
+            var thumbTip = _fingerProvider.ThumbTip;
+            var indexTip = _fingerProvider.IndexTip;
+
+            if (Vector3.Dot(thumbTip.right, indexTip.right) < _fingerData.lShapeThreshold)
+            {
+                if (!_isMakingLShape)
+                {
+                    LShapeCreatedCallack?.Invoke(OVRInput.Handedness.RightHanded);
+                    _isMakingLShape = true;
+                }
+            }
+            else
+            {
+                _isMakingLShape = false;
+            }
+        }
+        
+        // Understand when index + thumb are in close proximity
+
+        // Understand when Index and thumb make 'L' or right angle
+
+        // Understand when palm is up
+    }
+}
+
